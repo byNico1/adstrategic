72815a507466ee9cb039eaab25e26560248e9050
byNico1 <nicofoque0205@gmail.com>
2024-08-01 12:30:10 -0500

    edit setup files and packages

--- lp-items.tsx
+++ lp-items.tsx
@@ -3,14 +3,14 @@ import Image from "next/image"
 export const LP_GRID_ITEMS = [
   {
     title: "Industry Specialists",
-    icon: <Image quality={100} src={"/advertising.png"} width={128} height={128} alt="" />,
+    icon: <Image quality={100} src={"/assets/icons/advertising.png"} width={128} height={128} alt="" />,
   },
   {
     title: "Results Guaranteed",
-    icon: <Image quality={100} src={"/guarantee-certificate.png"} width={128} height={128} alt="" />,
+    icon: <Image quality={100} src={"/assets/icons/guarantee-certificate.png"} width={128} height={128} alt="" />,
   },
   {
     title: "Qualified Leads",
-    icon: <Image quality={100} src={"/recruitment.png"} width={128} height={128} alt="" />,
+    icon: <Image quality={100} src={"/assets/icons/recruitment.png"} width={128} height={128} alt="" />,
   },
 ]
