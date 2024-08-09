486c3ce7c5595be27e76d16482fd068b0fecedf4
byNico1 <nicofoque0205@gmail.com>
2024-08-09 16:10:05 -0500

    add querys types

---
+++ app/types/posts.ts
@@ -0,0 +1,46 @@
+export interface Post {
+  seo: {
+    title: string
+    description: string
+  }
+  ogMetaData: {
+    image: string
+  }
+  author: {
+    name: string
+    profilePicture: string
+    socialMediaLinks: {
+      twitter: string
+    }
+  }
+  content: {
+    html: string
+  }
+  id: string
+  coverImage: {
+    url: string
+  }
+  publishedAt: string
+  slug: string
+  title: string
+}
+
+export interface QueryPostResult {
+  data: {
+    publication: {
+      post: Post
+    }
+  }
+}
+
+export interface QueryPostsList {
+  data: {
+    publication: {
+      posts: {
+        edges: Array<{
+          node: Post
+        }>
+      }
+    }
+  }
+}
