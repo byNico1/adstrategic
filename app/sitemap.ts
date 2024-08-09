ec8f72949bf8f13b8c5a3de05084c31f00c7e411
byNico1 <nicofoque0205@gmail.com>
2024-08-09 16:21:48 -0500

    add blog to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,36 @@
 import { MetadataRoute } from "next"
+import { Post, QueryPostsList } from "@/types/posts"
+import { query } from "@/utils/hashnode"
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const {
+    data: { publication },
+  } = (await query({
+    query: `
+    query($host: String!) {
+      publication(host: $host) {
+        posts(first: 10) {
+          edges {
+            node {
+              slug
+              publishedAt
+            }
+          }
+        }
+      }
+    }
+  `,
+    variables: {
+      host: "adstrategic.hashnode.dev",
+    },
+  })) as QueryPostsList
+
+  const posts: Array<Post> = publication.posts.edges.map(({ node }) => node)
+  const postEntries: MetadataRoute.Sitemap = posts.map(({ slug, publishedAt }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${slug}`,
+    lastModified: new Date(Date.parse(publishedAt)),
+  }))
+
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
@@ -8,5 +38,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+    ...postEntries,
   ]
 }
