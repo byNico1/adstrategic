9a034596e3c27a149244d0e602537f6aade4ad50
byNico1 <nicofoque0205@gmail.com>
2024-08-11 11:22:00 -0500

    add suspense for loading skeleton

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,60 +1,18 @@
 import type { Metadata } from "next"
 import Image from "next/image"
-import Link from "next/link"
+import { Suspense } from "react"
+import BlogList from "@/components/Blog/BlogList"
 import Container from "@/components/Container/Container"
+import LoadingSkeleton from "@/components/LoadingSkeleton"
 import Reveal from "@/components/Reveal"
-import type { Post, QueryPostsList } from "@/types/posts"
-import { query } from "@/utils/hashnode"
-import { unstable_noStore } from "next/cache"
-// fix image metadata
 
 export const metadata: Metadata = {
   title: "The Adstrategic Blog | Improve your Marketing and Software skills",
   description:
     "Learn and expand your Digital Marketing and Software Development knowledge with detailed tutoriales and examples.",
-  // openGraph: {
-  //   images: [
-  //     {
-  //       url: "/blog/cover-image.jpg",
-  //       width: 1200,
-  //       height: 630,
-  //     },
-  //   ],
-  // },
 }
 
-export default async function Blog() {
-  unstable_noStore()
-
-  const {
-    data: { publication },
-  } = (await query({
-    query: `
-    query ($host: String!) {
-      publication(host: $host) {
-        posts(first: 10) {
-         edges {
-            node {
-              coverImage {
-                url
-              }
-              id
-              publishedAt
-              slug
-              title
-            }
-          }
-        }
-      }
-    }
-  `,
-    variables: {
-      host: "adstrategic.hashnode.dev",
-    },
-  })) as QueryPostsList
-
-  const posts: Array<Post> = publication.posts.edges.map(({ node }: { node: Post }) => node)
-
+export default function Blog() {
   return (
     <>
       <section className="mt-24">
@@ -69,45 +27,13 @@ export default async function Blog() {
             </p>
           </Container>
           <div className="absolute bottom-[-100px] right-[calc(50%-150px)] z-0 blur-lg grayscale">
-            <img src="/adstrategic-180.png" width={300} height={300} alt="" />
+            <Image src="/adstrategic-180.png" width={300} height={300} alt="" />
           </div>
         </div>
 
-        <Container>
-          <ul className="grid grid-cols-1 gap-0 overflow-hidden rounded-md border border-black/10 sm:grid-cols-2 md:grid-cols-3">
-            {posts.map((post) => {
-              return (
-                <li
-                  key={post.id}
-                  className="cursor-pointer border-b border-black/10 p-6 hover:bg-black/10 [&:not(:nth-child(3n+3))]:border-r [&:nth-last-child(-n+3)]:border-b-0"
-                >
-                  <Link href={`/blog/${post.slug}`} className="mb-16 grid grid-cols-1 gap-8">
-                    <Image
-                      width="600"
-                      height="400"
-                      className="rounded border border-zinc-200"
-                      src={post.coverImage.url}
-                      alt=""
-                    />
-                    <div>
-                      <h2 className="mb-5 overflow-hidden text-ellipsis border-b-2 border-gray-400 pb-5 text-2xl font-bold uppercase leading-tight tracking-wide">
-                        {post.title}
-                      </h2>
-                      <p className="text-zinc-500">
-                        {new Date(post.publishedAt).toLocaleDateString("en-us", {
-                          weekday: "long",
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                        })}
-                      </p>
-                    </div>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </Container>
+        <Suspense fallback={<LoadingSkeleton />}>
+          <BlogList />
+        </Suspense>
       </section>
     </>
   )
